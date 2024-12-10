51ef63635a4eaa2116134b49f1560c5ba8642932
Zulu-inventor33 <muteakelvin15@gmail.com>
2024-12-10 13:18:51 +0000

    all

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,23 @@
-/* eslint-disable import/no-named-as-default */
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
-
-export default class AppController {
+class AppController {
+  /**
+   * Get the current status of the application.
+   * @param {Request} req - The incoming request object.
+   * @param {Response} res - The outgoing response object.
+   */
   static getStatus(req, res) {
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
+    res.status(200).json({ status: 'OK', message: 'Application is running smoothly.' });
   }
 
+  /**
+   * Get server statistics, such as uptime or request metrics.
+   * @param {Request} req - The incoming request object.
+   * @param {Response} res - The outgoing response object.
+   */
   static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+    // Assuming we gather stats from some service or environment.
+    res.status(200).json({ uptime: process.uptime(), message: 'Server statistics retrieved successfully.' });
   }
 }
+
+export default AppController; // Exporting the AppController class for use in routing.
+
